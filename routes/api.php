d29f863c4e2e38e9adaf0733d4b8c5dad7f335bd
chiapello <francolautarochiapello@gmail.com>
2024-01-28 10:33:36 -0300

    feat (UserController): Agregado de login & register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,9 +10,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', [ProductController::class,"index"]);
+});
+
+Route::post('auth/register', [UserController::class,"createUser"]);
+Route::post('auth/login', [UserController::class,"loginUser"]);
 
 Route::controller(ProductController::class)->group(function(){
-    Route::get('/products', 'index');
+    // Route::get('/products', 'index');
     Route::post('/product','store');
     Route::get('/product/{id}','show');
     Route::put('/product/{id}','update');
